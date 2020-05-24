4c511a3f3a9d0702077f09b10963a057fc41dd0f
anbesajer pawlos <ambesa4@gmail.co>
2020-05-24 16:12:41 -0700

    Updates

---
+++ precache-manifest.311b9481d4e371d64d2e7813ce509731.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08fd02a86b7c5f21c9ecae8daeba666",
+    "url": "/Mytodolist/index.html"
+  },
+  {
+    "revision": "fd1709770ce3369230c1",
+    "url": "/Mytodolist/static/css/main.2eb6323e.chunk.css"
+  },
+  {
+    "revision": "2753490be274190f97b0",
+    "url": "/Mytodolist/static/js/2.9bc38a1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mytodolist/static/js/2.9bc38a1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd1709770ce3369230c1",
+    "url": "/Mytodolist/static/js/main.1b2e6bf5.chunk.js"
+  },
+  {
+    "revision": "6de5ce9a3b0c86b84880",
+    "url": "/Mytodolist/static/js/runtime-main.2e3a14ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Mytodolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
